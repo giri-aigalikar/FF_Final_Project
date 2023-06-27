f8a95a75ca9b5c45cc37e9a1783a1542aab5422e
giri-aigalikar <giri.aigalikar@ipg-automotive.com>
2023-06-27 14:47:25 -0400

    commit after build

--- src/app_tmp.c
+++ src/app_tmp.c
@@ -56,10 +56,10 @@ static const char *CompileFlags[] = {
 
 tAppStartInfo   AppStartInfo = {
     "Car_Generic <insert.your.version.no>",          /* App_Version         */
-    "5",          /* App_BuildVersion    */
+    "6",          /* App_BuildVersion    */
     "giri",     /* App_CompileUser     */
     "giri-thinkpad-p15-gen-2i",         /* App_CompileSystem   */
-    "2023-06-27 13:47:37",  /* App_CompileTime */
+    "2023-06-27 14:47:24",  /* App_CompileTime */
 
     CompileFlags,                /* App_CompileFlags  */
     CompileLibs,                 /* App_Libs          */
